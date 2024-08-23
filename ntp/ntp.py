70f44ea7e05e53fb2e6572c8a54932535711bbc7
unknown <khandanamir7@gmail.com>
2024-08-23 13:22:32 +0330

    ntp.py error message update

--- ntp/ntp.py
+++ ntp/ntp.py
@@ -5,8 +5,8 @@ def current_time_ntp():
     c = ntplib.NTPClient()
     try:
         response = c.request("ir.pool.ntp.org", version=3)
-    except Exception as e:
-        print(e)
+    except Exception:
+        print("ir server didn't respond, trying asia...")
         response = c.request("2.asia.pool.ntp.org", version=3)
 
     ms_since_epoch = int(response.tx_time * 1000)
