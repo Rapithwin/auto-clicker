a1a870d796ea58d87601ddacc39beeaf1cedd96d
guest guest <g.guest@kanoon.iran>
2024-08-26 14:15:19 +0330

    Minor change in read_file.py

--- funcitons/read_file.py
+++ funcitons/read_file.py
@@ -4,8 +4,9 @@ import datetime
 def read_date_time() -> list:
     with open("conf.txt", "r") as f:
         file = f.read()
+        file_len = len(file)
         date_time_str = file[0:-4]
-        ms = file[20:25]
+        ms = file[20:file_len]
         format = r"%Y/%m/%d %H:%M:%S"
         date_time_obj = datetime.datetime.strptime(date_time_str, format)
     return date_time_obj, ms
